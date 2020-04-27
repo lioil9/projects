799d95f22454164e6e4b31a6002206a3253ad943
lioil9 <156949416@qq.com>
2020-04-27 21:02:31 +0800

    update

--- work_4_24/practice3/fingerguess/src/club/banyuan/Game.java
+++ work_4_24/practice3/fingerguess/src/club/banyuan/Game.java
@@ -1,58 +1,95 @@
-package club.banyuan;
+import java.util.Scanner;
 
-import club.banyuan.Player;
-import club.banyuan.Computer;
-
-class Game{
-
+class Game {
+    static Player[] playerList;
+    static String[] choices = {"剪刀", "石头", "布"};
+    static Player p;
+    static Scanner sc;
     /**
-     * 创建电脑数组
-     * @param num 人数
-     * @return 
+     * 输入定义电脑数量
+     * @param computer
      */
-    public Computer[] computers(int num){
-        Computer[] computerArray = new Computer[num-1];
-        for(int i=0; i<num-1; i++){
-            Computer computer = new Computer();
-            computerArray[i] = computer;
+    public static void getComputers(int computer){
+        playerList = new Player[computer+1];
+        playerList[0] = new Player("玩家"+1);
+        for(int i=1; i<=computer; i++){
+            playerList[i] = new Player("电脑"+i);
         }
-        return computerArray;
+    }
+    /**
+     * 得到电脑和玩家出拳
+     * @return
+     */
+    public static String getPlayersInput() {
+        String judge = "";
+        for (int i = 0; i < playerList.length; i++) {
+            p = playerList[i];
+            if (p.name.contains("电脑")) {
+                p.punch = (int)(Math.random() * 3) + 1;
+            } else {
+                System.out.print(p.name + "输入:");
+                p.punch = sc.nextInt();
+            }
+            //playerList.set(i, p);
+            if (!judge.contains(String.valueOf(p.punch))) {
+                judge += p.punch;
+            }
+        }
+        return judge;
+    }
+    /**
+     * 比较 1.剪刀 2.石头 3.布
+     * @param judge
+     */
+    public static void compare(String judge){
+        Player p1;
+        Player p2;
+        for (int i = 0; i < playerList.length; i++) {
+            p1 = playerList[i];
+            System.out.println(p1.name + "出 " + choices[p1.punch - 1]);
+            if (judge.length() == 2) {
+                for (int j = 0; j < playerList.length; j++) {
+                    p2 = playerList[j];
+                    if (p1.punch == 1 && p2.punch ==3) {
+                        p1.status = "赢了";
+                    }else if(p1.punch == 1 && p2.punch ==2){
+                        p1.status = "输了";
+                    }else if(p1.punch == 2 && p2.punch ==1){
+                        p1.status = "赢了";
+                    }else if(p1.punch == 2 && p2.punch ==3){
+                        p1.status = "输了";
+                    }else if(p1.punch == 3 && p2.punch ==1){
+                        p1.status = "输了";
+                    }else if(p1.punch == 3 && p2.punch ==2){
+                        p1.status = "赢了";
+                    } else {
+                        p1.status = "平手";
+                    }
+                }
+            }
+        }
     }
 
-    public int[] guessing(Player player, int num){
-        int[] guessArray = new int[num];
-        int[] count = {0,0,0};
+    public void removeLoser() {
+        
+        if (playerList.length > 1) {
+            for (int i = 0; i < playerList.length; i++) {
+                p = playerList[i];
+                if (p.status.contains("输了")) {
+                    System.out.println(p.name + " 出局");
+                    i--;
+                }
+            }
+        }
+    }
+    
 
-        guessArray[0] = player.guess();
-        for(int i=1; i<num; i++){
-            guessArray[i] = Game.computerArray[i-1].guess;
-        }
-        //判断数组中剪刀石头布分布
-        for(int i=0; i<num; i++){
-            switch(guessArray[i]){
-                case 1:
-                    count[0]++;
-                    break;
-                case 2:
-                    count[1]++;
-                    break;
-                case 3:
-                    coutn[2]++;
-                    break;
-            }
-        }
-        for(int i=0; i<count.length; i++){
-            if(count[i] == 0){
-                
-            }
-        }
+
+    public static void main(String[] args) {
+        int round = 1;
+
     }
 
     
-    public static void main(String[] args) {
-        Computer computer1 = new Computer();
-        Computer computer2 = new Computer();
-        System.out.println(computer1.getName());
-        System.out.println(computer2.getName());
-    }
+
 }
