b67051c7c048af25aed0b4743c5e4e9ddb5f835b
lioil <2398943512>
2020-05-11 23:28:56 +0800

    update

--- work_5_11/DataSet/src/club/banyuan/Country.java
+++ work_5_11/DataSet/src/club/banyuan/Country.java
@@ -2,15 +2,15 @@ package club.banyuan;
 
 // TODO: 实现 Measurable 接口. getMeasure() 返回国家的人口总数. 提供构造方法，让DataSetTester运行正常
 public class Country implements Measurable{
-    private final int measure;
+    private final int population;
 
-    public Country(int measure){
-        this.measure = measure;
+    public Country(int population){
+        this.population = population;
     }
 
     @Override
     public double getMeasure() {
-        return measure;
+        return population;
     }
 
 
