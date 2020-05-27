b482857a8007ac42ea9bf9a93e4f2d70b1941954
lioil9 <156949416@qq.com>
2020-05-27 21:06:08 +0800

    update

---
+++ work_5_27/vms/src/club/banyuan/main/Main.java
@@ -0,0 +1,53 @@
+package club.banyuan.main;
+
+import club.banyuan.machine.FlowStatus;
+import club.banyuan.machine.VendingMachine;
+import club.banyuan.machine.VendingMachineWithMenu;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // VendingMachine vendingMachine = new VendingMachine();
+    // vendingMachine.start();
+    VendingMachineWithMenu vendingMachineWithMenu = deserializeVendingMachine();
+    vendingMachineWithMenu.setFlowStatus(FlowStatus.ROOT);
+    if(vendingMachineWithMenu == null) {
+      vendingMachineWithMenu = new VendingMachineWithMenu();
+    }
+    vendingMachineWithMenu.start();
+//    serializeVendingMachine(vendingMachineWithMenu);
+  }
+
+  public static void serializeVendingMachine(VendingMachineWithMenu vms) {
+    try {
+      ObjectOutputStream oos = new ObjectOutputStream(
+          new FileOutputStream(new File("./information/vms.txt")));
+      oos.writeObject(vms);
+      oos.flush();
+      oos.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static VendingMachineWithMenu deserializeVendingMachine() {
+    try {
+      ObjectInputStream ois = new ObjectInputStream(
+          new FileInputStream(new File("./information/vms.txt")));
+      VendingMachineWithMenu vms = (VendingMachineWithMenu) ois.readObject();
+      return vms;
+    } catch (IOException | ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+
+}
